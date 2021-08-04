ddd390dcd3abff380bf858ecee43ba88f69569f9
EC2 Default User <ec2-user@ip-172-31-36-3.us-east-2.compute.internal>
2021-08-04 05:35:04 +0000

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index(){
+        // ユーザをID順で降順に9個まで表示する
+        $users = User::orderBy('id','desc')->paginate(9);
+        
+        return view('welcome',['users' => $users]);
+    }
+    
+    public function show($id)
+    {
+        $user = User::find($id);
+        $movies = $user->movies()->orderBy('id','desc')->paginate(9);
+        
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+            ];
+            
+        $data += $this -> counts($user);
+        
+        return view('users.show', $data);
+    }
+    
+    public function rename(Request $request)
+    {
+        $this->validate($request,[
+            'channel' => 'required|max:15',
+            'name' => 'required|max:15',
+            ]);
+            
+        $user=\Auth::user();
+        $movies=$user->movies()->orderBy('id','desc')->paginate(9);
+        
+        $user->channel = $request->channel;
+        $user->name = $request->name;
+        $user->save();
+        
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+            ];
+            
+        $data += $this->counts($user);
+        
+        return view('users.show',$data);
+    }
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(9);
+        
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+        
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+    }
+    
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(9);
+        
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+        
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
+    }
+}
---
+++ resources/views/users/followers.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('users.tabs',['user' => $user])
+    @include('users.users',['users' => $users])
+@endsection
